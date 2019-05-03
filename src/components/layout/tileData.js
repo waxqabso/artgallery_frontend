9a77f5320a0eaf247eb050425ea4db988a567176
Ashwaq <ashwaqismail06@gmail.com>
2019-05-03 04:56:37 -0700

    add the header and layout

---
+++ src/components/layout/tileData.js
@@ -0,0 +1,12 @@
+const tileData = [
+    {
+        img: '/static/images/grid-list/breakfast.jpg',
+        title: 'Breakfast',
+        author: 'jill111',
+        cols: 2,
+        featured: true,
+    },
+
+];
+
+export default tileData;
