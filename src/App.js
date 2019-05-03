18c163dd4c10a742a949bf097fc6f09e8a7f1f0e
Ashwaq <ashwaqismail06@gmail.com>
2019-05-03 02:25:45 -0700

    fix the problem

--- src/App.js
+++ src/App.js
@@ -1,21 +1,14 @@
-import React from 'react';
-import './App.css';
-<<<<<<< HEAD
-import MrArtr from './components/home/mrArt';
+import React from "react";
+import "./App.css";
+import MrArtr from "./components/home/mrArt";
+import Header from "./components/layout/Header";
+import Layout from "./components/layout/Layout";
 function App() {
   return (
     <div className="App">
-     <MrArtr/>
-=======
-import Header from './components/layout/Header';
-import Layout from './components/layout/Layout';
-
-function App() {
-  return (
-    <div className="App">
-      <Header/>
-      <Layout/>
->>>>>>> end the header part and layout updated
+      {/* <MrArtr/> */}
+      <Header />
+      <Layout />
     </div>
   );
 }
