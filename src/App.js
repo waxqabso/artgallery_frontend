a86a0dbf31a684fa4e7677e4950ef19a34a717b8
Ashwaq <ashwaqismail06@gmail.com>
2019-05-04 10:22:42 -0700

    this was a mistake that i do

--- src/App.js
+++ src/App.js
@@ -3,12 +3,12 @@ import "./App.css";
 import MrArtr from "./components/home/mrArt";
 import Header from "./components/layout/Header";
 import Layout from "./components/layout/Layout";
+
+import Gallery from "./components/Gallery/Gallery";
 function App() {
   return (
     <div className="App">
-      <Header />
-      <Layout />
-      <MrArtr />
+      <Gallery />
     </div>
   );
 }
